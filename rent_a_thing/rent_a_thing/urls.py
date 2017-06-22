6bbaad5d6a337a56bb8af96304f0e764eafecebd
João Gabriel Miranda <jg.miranda@outlook.com>
2017-06-21 23:34:42 -0300

    Configuração inicial do django rest framework

--- rent_a_thing/rent_a_thing/urls.py
+++ rent_a_thing/rent_a_thing/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import include,url
 from django.contrib import admin
+from server import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^server/api/', include('server.urls')),
+    #url(r'^server/api/', include('server.urls')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))    
 ]
