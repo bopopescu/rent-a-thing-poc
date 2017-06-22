2a5485ca5d8523eebb66350fca7ed1231c37f061
João Gabriel Miranda <jg.miranda@outlook.com>
2017-06-22 10:16:24 -0300

    Adição do modelo que representa os clientes ligados ao módulo servidor

--- rent_a_thing/server/models.py
+++ rent_a_thing/server/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Clients(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeFIeld(auto_now=True)
+    description = models.CharField(max_length=150, blank=False)
+    Address = models.CharField(max_length=500, blank=False)
+    HostAddress = models.CharField(max_length=150, blank=False)
