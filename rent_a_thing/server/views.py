c837121e273ea6cbb4b037f70b3cb7c15d912ca3
João Gabriel Miranda <jg.miranda@outlook.com>
2017-06-23 11:06:33 -0300

    Crção do endpoint de listagem

--- rent_a_thing/server/views.py
+++ rent_a_thing/server/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
+from django.views.decorators.csrf import csrf_exempt
 from server.serializers import UserSerializer, GroupSerializer, ClientSerializer
 from server.models import Client
+from django.http import HttpResponse, JsonResponse
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
@@ -14,3 +18,10 @@ class GroupViewSet(viewsets.ModelViewSet):
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+
+    @csrf_exempt
+    def list(request):
+        if request.method == 'GET':
+            clients = Client.objects.all()
+            serializer = ClientSerializer(clients, many=True)
+            return JsonResponse(serializer.data, safe=False)
