74a0c21705b0f68b61260eb5bb581ccb7eed50fb
João Gabriel Miranda <jg.miranda@outlook.com>
2017-07-05 00:06:43 -0300

    Adição do objeto rental

--- rent_a_thing/core/models.py
+++ rent_a_thing/core/models.py
@@ -1,3 +1,23 @@
+import uuid
+from django.contrib.auth.models import User
 from django.db import models
-
+from server.models import Client
 # Create your models here.
+
+class RentalObject (models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
+    created = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+    current_tenant_user = models.OneToOneField(User, on_delete=models.PROTECT)
+    
+class Rental (models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+    tenant_user = models.OneToOneField(User, on_delete=models.PROTECT)
+    rented_object = models.OneToOneField(RentalObject, on_delete=models.PROTECT)
+    is_confirmed = models.BooleanField(default=True)
+    rental_station = models.OneToOneField(Client, on_delete=models.PROTECT)
+    return_station = models.OneToOneField(Client, on_delete=models.PROTECT)
+    rental_date = models.DateTimeField()
+    return_date = models.DateTimeField()
+    
