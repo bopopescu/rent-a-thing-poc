1c3f09fa69d5e6036f2239aac192f8e320264a7d
João Gabriel Miranda <jg.miranda@outlook.com>
2017-07-06 23:38:55 -0300

    refactor

--- rent_a_thing/core/views.py
+++ rent_a_thing/core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from core.serializers import UserSerializer, GroupSerializer
 from django.contrib.auth.models import User, Group
+from django.http import HttpResponse, JsonResponse
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
 
