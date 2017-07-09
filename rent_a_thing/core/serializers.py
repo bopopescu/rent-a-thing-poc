4b27bf768ad3e2376b557363b1c6972863c78e24
João Gabriel Miranda <jg.miranda@outlook.com>
2017-07-08 23:10:34 -0300

    refactor

--- rent_a_thing/core/serializers.py
+++ rent_a_thing/core/serializers.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from core.models import Rental
+from core.models import Rental, RentalObject
+from server.serializers import ClientSerializer
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -13,17 +14,32 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         model = Group
         fields = ('url', 'name')
 
+class RentalObjectSerializer(serializers.Serializer):
+    id = serializers.UUIDField(format='hex_verbose')
+    current_tenant_user = serializers.UserSerializer(many=False)
+
 class RentalSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    tenant_user = 
+    tenant_user = UserSerializer(many=False)
+    rented_object = RentalObjectSerializer(many=False)
+    is_confirmed = serializers.BooleanField()
+    rental_station = ClientSerializer(many=False)
+    return_station = ClientSerializer(many=False)
+    rental_date = serializers.DateTimeField()
+    return_date = serializers.DateTimeField()
+
 
     def create(self, validated_data):
-        return Client.objects.create(**validated_data)
+        return Rental.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.description = validated_data.get('description', instance.description)
-        instance.address = validated_data.get('address', instance.address)
-        instance.host_address = validated_data.get('host_address', instance.host_address)
+        instance.tenant_user = validated_data.get('tenant_user', instance.tenant_user)
+        instance.rented_object = validated_data.get('rented_object', instance.rented_object)
+        instance.is_confirmed = validated_data.get('is_confirmed', instance.is_confirmed)
+        instance.rental_station = validated_data.get('rental_station', instance.rental_station)
+        instance.rental_date = validated_data.get('rental_date', instance.rental_date)
+        instance.return_station = validated_data.get('return_station', instance.return_station)
+        instance.return_date = validated_data.get('return_date', instance.return_date)
 
         instance.save()
         return instance
